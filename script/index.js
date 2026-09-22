c73a32f7980b912b5dfeaaadb8236c786b6a297c
sanjanaoney <iamoney012@gmail.com>
2026-09-23 00:39:00 +0600

    categories-implemented

--- script/index.js
+++ script/index.js
@@ -1,36 +1,19 @@
-// const removeActive=()=>{
-//   const allTreesButton=document.querySelectorAll(".category-btn")
-//   allTreesButton.forEach(btn=> btn.classList.remove("active"))
-// }
 const removeActive = () => {
   const allCategoryButtons = document.querySelectorAll(".category-btn");
-
   allCategoryButtons.forEach((btn) => {
     btn.classList.remove("bg-[#15803D]", "text-white");
+    // btn.classList.add("text-[#1F2937]", "hover:text-[#15803D]");
+    btn.classList.add("!bg-transparent", "hover:!bg-transparent", "text-[#1F2937]", "hover:text-[#15803D]");
   });
 };
 
-
-// const loadPlants=()=>{
-//   removeActive();
-//   document.getElementById("all-trees-btn").classList.add("active")
-//     const url=`https://openapi.programming-hero.com/api/plants`
-//     fetch(url)
-//     .then((res)=>res.json())
-//     .then((data)=>{
-//         displayPlants(data.plants)
-//     })
-// }
-
 const loadPlants = () => {
   removeActive();
-
-  document
-    .getElementById("all-trees-btn")
-    .classList.add("bg-[#15803D]", "text-white");
+  const allTreesBtn = document.getElementById("all-trees-btn");
+  allTreesBtn.classList.remove("!bg-transparent", "hover:!bg-transparent", "text-[#1F2937]", "hover:text-[#15803D]");
+  allTreesBtn.classList.add("bg-[#15803D]", "text-white");
 
   const url = `https://openapi.programming-hero.com/api/plants`;
-
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
@@ -38,23 +21,22 @@ const loadPlants = () => {
     });
 };
 
-const loadPlantDetail=(id)=>{
-  const url=`https://openapi.programming-hero.com/api/plant/${id}`
+const loadPlantDetail = (id) => {
+  const url = `https://openapi.programming-hero.com/api/plant/${id}`;
   fetch(url)
-  .then((res)=>res.json())
-  .then((data)=>{
- displayPlantDetails(data.plants);
-  })
-}
+    .then((res) => res.json())
+    .then((data) => {
+      displayPlantDetails(data.plants);
+    });
+};
 
-const displayPlants=(plants)=>{
-    const cardContainer=document.getElementById("card-container")
-    cardContainer.innerHTML="";
-    plants.forEach((plant)=>{
-        const card=document.createElement("div");
-        card.innerHTML=`
-        <div class="flex flex-col bg-white rounded-lg shadow p-4 w-full h-full transition duration-200 hover:scale-105 hover:shadow-xl hover:bg-[#FEFCE8] ">
-       
+const displayPlants = (plants) => {
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
+  plants.forEach((plant) => {
+    const card = document.createElement("div");
+    card.innerHTML = `
+        <div class="flex flex-col bg-white rounded-lg shadow p-4 w-full h-full transition duration-200 hover:scale-105 hover:shadow-xl hover:bg-[#FEFCE8]">
 
   <div class="image-container">
     <img src="${plant.image}" alt="" class="rounded-md w-full h-40 object-cover">
@@ -69,7 +51,7 @@ const displayPlants=(plants)=>{
   </div>
 
   <div class="flex justify-between items-center mt-3">
-    <div class="tree-category bg-[#DCFCE7] rounded-xl px-2 py-1 whitespace-nowrap ">
+    <div class="tree-category bg-[#DCFCE7] rounded-xl px-2 py-1 whitespace-nowrap">
       <p class="text-[#15803D] text-center text-sm">${plant.category}</p>
     </div>
     <div class="font-semibold">
@@ -84,17 +66,14 @@ const displayPlants=(plants)=>{
   </div>
 
 </div>
-
         `;
-        cardContainer.append(card);
-    })
-}
-loadPlants
-();
+    cardContainer.append(card);
+  });
+};
 
- const displayPlantDetails=(plant)=>{
-  const detailsBox=document.getElementById("plant-details-container")
-  detailsBox.innerHTML=`
+const displayPlantDetails = (plant) => {
+  const detailsBox = document.getElementById("plant-details-container");
+  detailsBox.innerHTML = `
 <div>
   <img src="${plant.image}" alt="">
 </div>
@@ -111,51 +90,47 @@ loadPlants
 <div>
   <p>Price: <i class="fa-solid fa-bangladeshi-taka-sign"></i>${plant.price}</p>
 </div>
-  `
+  `;
   document.getElementById("plant_modal").showModal();
- }
- const loadCategories=()=>{
-  const url=`https://openapi.programming-hero.com/api/categories`
+};
+
+const loadCategories = () => {
+  const url = `https://openapi.programming-hero.com/api/categories`;
   fetch(url)
-  .then((res)=>res.json())
-  .then((data)=>{
-    displayCategories(data.categories);
-  })
- }
+    .then((res) => res.json())
+    .then((data) => {
+      displayCategories(data.categories);
+    });
+};
 
 const displayCategories = (categories) => {
-  const categoryContainer =
-    document.getElementById("category-container");
-
+  const categoryContainer = document.getElementById("category-container");
   categoryContainer.innerHTML = "";
 
   categories.forEach((category) => {
     const button = document.createElement("button");
-
     button.innerText = category.category_name;
-
-    button.className = "category-btn text-left text-black";
+    button.className = "category-btn btn btn-block justify-start text-left text-sm !bg-transparent hover:!bg-transparent border-none mt-2 text-[#1F2937] hover:text-[#15803D]";
 
     button.onclick = () => {
       removeActive();
-
+      button.classList.remove("!bg-transparent", "hover:!bg-transparent", "text-[#1F2937]", "hover:text-[#15803D]");
       button.classList.add("bg-[#15803D]", "text-white");
-
       loadCategory(category.id);
     };
 
     categoryContainer.append(button);
   });
 };
-const loadCategory = (id) => {
-  const url =
-    `https://openapi.programming-hero.com/api/category/${id}`;
 
+const loadCategory = (id) => {
+  const url = `https://openapi.programming-hero.com/api/category/${id}`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
       displayPlants(data.plants);
     });
 };
+
 loadPlants();
 loadCategories();
